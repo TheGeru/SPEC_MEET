5ccc69c6b0fc1feaa3956e5a9718db503b272ce4
Jocelyn-MB <mancillablancasjocelyn@gmail.com>
2026-03-09 09:30:16 -0600

    vite.config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@shared': path.resolve(__dirname, 'src/shared'),
+      '@infrastructure': path.resolve(__dirname, 'src/infrastructure'),
+    },
+  },
 })
