b26d8759eb4af9175be0c4cf4d6e552a105721c2
Brandon Alonso Salinas <thegeru2prx@gmail.com>
2026-04-09 14:29:47 -0600

    Fix: import de bcrypt

--- server/src/controllers/user_controllers.ts
+++ server/src/controllers/user_controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { prisma } from "../config/prisma";
+import bcrypt from 'bcryptjs'
 
 export const getUsersWithReservations = async (req: Request, res: Response): Promise<void> => {
     try {
